09c8146bae96bdf232c68af21f34f0079e9c3993
lenazelenaya <lena.zelenaya@gmail.com>
2020-07-22 01:41:14 +0300

    Add message list. Hope it will work

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Message from "../../types/message";
+import PropTypes from "prop-types";
+import MessageC from '../Message/index'
+import ms from '../../services/messageService'
+
+interface ListState {}
+
+interface ListProps {
+  user: string;
+  messages: Message[];
+  addLike: Function;
+  editMessage: Function;
+  deleteMessage: Function;
+}
+
+class MessageList extends React.Component<ListProps, ListState> {
+  static propTypes = {
+    user: PropTypes.string,
+    messages: PropTypes.array,
+    addLike: PropTypes.func,
+    editMessage: PropTypes.func,
+    deleteMessage: PropTypes.func,
+  };
+  constructor(props: ListProps){
+    super(props);
+  }
+
+  render() {
+    return (
+        <div className="message-list">
+            {ms.groupByDate(this.props.messages).map((groupsByDate, id) => (
+                <div key={id}>
+                <div className="divider">{groupsByDate.date}</div>
+                {groupsByDate.messages.map((message: Message, id: string) => (
+                    <MessageC
+                        id = {id}
+                        user = {this.props.user}
+                        message = {message}
+                        addLike = {this.props.addLike}
+                        editMessage = {this.props.editMessage}
+                        deleteMessage = {this.props.deleteMessage}
+                    />
+                ))}
+                </div>
+            ))}
+        </div>
+    )
+  }
+}
+
+export default MessageList;
